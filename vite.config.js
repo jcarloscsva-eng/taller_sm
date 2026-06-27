d8f197a4df233556277e675a530e8602ce4be656
jcarloscsva-eng <jcarloscsva@gmail.com>
2026-06-28 00:26:33 +0200

    Base URL para dominio propio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia 'taller-os' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/taller_sm/',
+  base: '/',
 })
